0041f207f06eb52ed92c02bf7499f0f850a987c4
Gehm-Nicolas <nicolas.gehm@gmail.com>
2017-05-09 09:54:34 -0300

    Adding Arduino codes

---
+++ DCMotor_Dynamixel.ino
@@ -0,0 +1,184 @@
+#include "XL320.h"
+#include "DCMotor.h"
+#include "Encoder.h"
+
+//GLOBAL DEFINITIONS
+#define LEFT_MOTOR_ID   9 
+#define RIGHT_MOTOR_ID  10
+#define UPDATE_TIME     50//50ms
+/*ERRORS
+DXL_ERROR_OVERLOAD              1
+DXL_ERROR_OVER_HEATING          2
+DXL_ERROR_INPUT_VOLTAGE         3
+DXL_ERROR_INVALID_INSTRUCTION   4*/
+
+//******************//
+//* ARDUINO PINOUT *//
+//******************//
+#define RX              1
+#define TX              0
+#define STBY            9
+#define WRITE_CONTROL   13
+//LEFT MOTOR
+#define LEFT_ENCODER_1  3
+#define LEFT_ENCODER_2  5
+#define PWMA            6
+#define AIN2            7 //A02 -> M2
+#define AIN1            8 //A01 -> M1
+//RIGHT MOTOR
+#define RIGHT_ENCODER_1 2
+#define RIGHT_ENCODER_2 4
+#define PWMB            10
+#define BIN2            11 //B02 -> M2
+#define BIN1            12 //B01 -> M1
+
+XL320 dxl(1000000,LEFT_MOTOR_ID,RIGHT_MOTOR_ID,Serial);
+Encoder encoder_L(LEFT_ENCODER_1,LEFT_ENCODER_2);
+DCMotor motor_L(LEFT_MOTOR_ID,AIN2,AIN1,PWMA,STBY,encoder_L);
+Encoder encoder_R(RIGHT_ENCODER_1,RIGHT_ENCODER_2);
+DCMotor motor_R(RIGHT_MOTOR_ID,BIN2,BIN1,PWMB,STBY,encoder_R);
+
+unsigned long lastMilli = 0;//loop timer
+
+void setup()
+{
+  pinMode(WRITE_CONTROL,OUTPUT);
+  digitalWrite(WRITE_CONTROL,HIGH);//HIGH => disable Arduino write/send.
+  dxl.init();
+}
+
+void loop()
+{
+  dxl.checkMessages();
+  if(dxl.instruction != DXL_NO_DATA)
+  {
+    switch(dxl.instruction)
+    {
+      case DXL_PING:break;
+      case DXL_READ_DATA: dxlReadData();
+                          break;
+      case DXL_WRITE_DATA:dxlWriteData();
+                          break;
+      case DXL_RESET:break;
+      case DXL_SYNC_WRITE:break;
+      default:break;
+    }
+  }else{
+    //Do nothing
+  }
+  
+  if((millis() - lastMilli) >= UPDATE_TIME)
+  {
+    lastMilli = millis();
+    motor_R.encoderUpdate();
+    motor_L.encoderUpdate();
+    motor_R.speedUpdate();
+    motor_L.speedUpdate();
+  }
+  motor_R.move();
+  motor_L.move();
+}
+
+void dxlReadData(){
+  int memAddress = 0;
+  int dataLen = 0;
+  unsigned int data = 0;
+  unsigned char error = 0;
+  memAddress = dxl.parameters[0];
+  memAddress += ((dxl.parameters[1] & 0x00FF) << 8);
+  dataLen = dxl.parameters[2];
+  dataLen += ((dxl.parameters[3] & 0x00FF) << 8);
+
+  if(dxl.received_id == motor_L.getID())//LEFT_MOTOR_ID
+  {
+    switch(memAddress)
+    {
+      case DXL_ID:              data = motor_L.getID();
+                                break;
+      case DXL_PRESENT_POSITION:data = abs(motor_L.getEncoder());
+                                break;    
+      case DXL_GOAL_SPEED:      data = motor_L.getGoalSpeed();
+                                break; 
+      case DXL_PRESENT_SPEED:   data = motor_L.getAvgSpeed();
+                                break;
+      case DXL_PRESENT_VOLTAGE: data = motor_L.getPWM();  
+                                break;
+      default:                  error = DXL_ERROR_INVALID_INSTRUCTION;
+                                break;
+    }  
+  }else{//RIGHT_MOTOR_ID
+    switch(memAddress)
+    {
+      case DXL_ID:              data = motor_R.getID();
+                                break;
+      case DXL_PRESENT_POSITION:data = abs(motor_R.getEncoder());
+                                break;    
+      case DXL_GOAL_SPEED:      data = motor_R.getGoalSpeed();
+                                break; 
+      case DXL_PRESENT_SPEED:   data = motor_R.getAvgSpeed();
+                                break;
+      case DXL_PRESENT_VOLTAGE: data= motor_R.getPWM();
+                                break;
+      default:                  error = DXL_ERROR_INVALID_INSTRUCTION;
+                                break;
+    }    
+  }
+  digitalWrite(WRITE_CONTROL,LOW);//enable Arduino write
+  dxl.sendStatusPacket(dxl.received_id,dxl.instruction,memAddress,error,data);
+  digitalWrite(WRITE_CONTROL,HIGH);//disable Arduino write
+}
+
+
+
+
+
+
+
+
+
+
+void dxlWriteData(){
+  unsigned char error = 0;
+  unsigned int data = 0;
+  int memAddress = 0;
+    
+  memAddress = dxl.parameters[0];
+  memAddress += ((dxl.parameters[1] & 0x00FF)<< 8);
+  if(dxl.total_parameters == 4){//2 length_bytes + 2 data_bytes
+    data = dxl.parameters[2];
+    data += ((dxl.parameters[3] & 0x00FF)<< 8);
+  }else{//2 length_bytes + 1 data_byte
+    data = dxl.parameters[2];
+  }
+
+  if(dxl.received_id == motor_L.getID()){//LEFT_MOTOR_ID
+    switch(memAddress)
+    {
+      case DXL_ID:              dxl.left_id = data;
+                                motor_L.setID(data);
+                                break;
+      case DXL_GOAL_SPEED:      motor_L.setGoalSpeed(data);//GOAL_SPEED(0 to 10)
+                                break;
+      case DXL_PRESENT_POSITION:motor_L.setEncoder(data);
+                                break;
+      default:                  error = DXL_ERROR_INVALID_INSTRUCTION;
+                                break;
+    }  
+  }else{//RIGHT_MOTOR_ID
+    switch(memAddress)
+    {
+      case DXL_ID:              dxl.right_id = data;
+                                motor_R.setID(data);
+                                break;
+      case DXL_GOAL_SPEED:      motor_R.setGoalSpeed(data);//GOAL_SPEED(0 to 10)
+                                break;
+      case DXL_PRESENT_POSITION:motor_R.setEncoder(data);
+                                break;                              
+      default:                  error = DXL_ERROR_INVALID_INSTRUCTION;
+                                break;
+    }  
+  }
+  digitalWrite(WRITE_CONTROL,LOW);//enable Arduino write
+  dxl.sendStatusPacket(dxl.received_id,dxl.instruction,memAddress,error,data);
+  digitalWrite(WRITE_CONTROL,HIGH);//disable Arduino write
+}
---
+++ DCMotor_Dynamixel_tests.ino
@@ -0,0 +1,116 @@
+#include "XL320.h"
+#include "DCMotor.h"
+#include "Encoder.h"
+
+//GLOBAL DEFINITIONS
+#define LEFT_MOTOR_ID   9 
+#define RIGHT_MOTOR_ID  10
+#define UPDATE_TIME     50//50ms
+#define DXL_ERROR_OVERLOAD              1
+#define DXL_ERROR_OVER_HEATING          2
+#define DXL_ERROR_INPUT_VOLTAGE         3
+#define DXL_ERROR_INVALID_INSTRUCTION   4
+
+//******************//
+//* ARDUINO PINOUT *//
+//******************//
+#define RX              1
+#define TX              0
+#define STBY            9
+#define WRITE_CONTROL   13
+//LEFT MOTOR
+#define LEFT_ENCODER_1  3
+#define LEFT_ENCODER_2  5
+#define PWMA            6
+#define AIN2            7 //A02 -> M2
+#define AIN1            8 //A01 -> M1
+//RIGHT MOTOR
+#define RIGHT_ENCODER_1 2
+#define RIGHT_ENCODER_2 4
+#define PWMB            10
+#define BIN2            11 //B02 -> M2
+#define BIN1            12 //B01 -> M1
+
+//XL320 dxl(1000000,LEFT_MOTOR_ID,RIGHT_MOTOR_ID,Serial);
+
+Encoder encoder_L(LEFT_ENCODER_1,LEFT_ENCODER_2);
+DCMotor motor_L(LEFT_MOTOR_ID,AIN2,AIN1,PWMA,STBY,encoder_L);
+
+Encoder encoder_R(RIGHT_ENCODER_1,RIGHT_ENCODER_2);
+DCMotor motor_R(RIGHT_MOTOR_ID,BIN2,BIN1,PWMB,STBY,encoder_R);
+
+
+void setup()
+{
+  pinMode(WRITE_CONTROL,OUTPUT);
+  digitalWrite(WRITE_CONTROL,HIGH);//HIGH => disable Arduino write/send.
+  //dxl.init();
+  Serial.begin(112500);
+}
+
+int vel = 20;//-10 to 10
+bool flag_move = true;
+unsigned long lastMilli = 0;//loop timer
+long wheels_diff = 0;
+
+void loop(){
+  if((millis() - lastMilli) >= UPDATE_TIME)
+  {
+    lastMilli = millis();
+
+    motor_R.encoderUpdate();
+    motor_L.encoderUpdate();
+    motor_R.speedUpdate();
+    motor_L.speedUpdate();
+  }
+  motor_R.move();
+  motor_L.move();
+  
+  if(flag_move){
+    motor_L.setGoalSpeed(vel);
+    motor_R.setGoalSpeed(vel);   
+    //motor_L.pwm_value = pwm;
+    //motor_L.move();
+    //motor_L.encoderUpdate();
+    //motor_L.speedUpdate();    
+    //motor_R.pwm_value = pwm;
+    //motor_R.move();
+    //motor_R.encoderUpdate();
+    //motor_R.speedUpdate();
+    flag_move = false;
+  }
+  if(motor_R.present_encoder_pos >= 3292){
+    motor_R.setGoalSpeed(10);
+    motor_L.setGoalSpeed(10);
+  }
+
+  if(millis() <= 5000){
+    //Data to MATLAB
+    Serial.print(millis());
+    //Serial.print(14*motor_L.goal_speed);
+    Serial.print("\t");
+    Serial.print(motor_L.present_encoder_pos);
+    Serial.print("_Avg");
+    //Serial.print(motor_L.present_encoder_pos-motor_L.last_encoder_pos);
+    Serial.print(motor_L.avg_speed);
+    Serial.print("_(");
+    Serial.print(motor_L.error);
+    Serial.print("::");
+    Serial.print(motor_L.error_acc);
+    Serial.print(")_");    
+    Serial.print(motor_L.pwm_value);  
+    
+    Serial.print("\t\t");
+
+    Serial.print(motor_R.present_encoder_pos);
+    Serial.print("_Avg");
+    //Serial.print(motor_R.present_encoder_pos-motor_R.last_encoder_pos);
+    Serial.print(motor_R.avg_speed);
+    Serial.print("_(");
+    Serial.print(motor_R.error);
+    Serial.print("::");
+    Serial.print(motor_R.error_acc);
+    Serial.print(")_");
+    Serial.println(motor_R.pwm_value);
+  }
+}
